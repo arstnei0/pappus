da1aeec718eaa8dc8380b7078e4141fcf901ac31
Zihan Chen <me@zihan.ga>
2022-10-01 03:02:25 +0000

    Update

--- scripts/install.mjs
+++ scripts/install.mjs
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
 import { resolve } from 'path';
 import { stderr } from 'process';
 
@@ -7,17 +7,13 @@ const cwd  = process.cwd();
 console.log('Start installing all packages...');
 
 // Install api depenencies
-exec('npm i', {
+const apiOut = execSync('npm i', {
     cwd: resolve(cwd, 'packages/api'),
-}, (err, stdout, stderr) => {
-    if (err) throw err;
-    console.log(stdout);
 });
+console.log(apiOut);
 
 // Install web dependencies
-exec('npm i', {
+const webOut = execSync('npm i', {
     cwd: resolve(cwd, 'packages/web'),
-}, (err, stdout, stderr) => {
-    if (err) throw err;
-    console.log(stdout);
 });
+console.log(webOut);
